617bce267c2dc61297f0fa54f7935c2b925beb04
GloriaMoraga <silmeriel@gail.com>
2019-01-30 16:52:08 -0300

    add firebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,9 @@
-const miFuncionTesteable = () => {
-return true;
-
-}
+let config = {
+    apiKey: "AIzaSyDoT3mAySwC3M65_EQGASJAm5szC9AuNaQ",
+    authDomain: "pruebafirebase-92d46.firebaseapp.com",
+    databaseURL: "https://pruebafirebase-92d46.firebaseio.com",
+    projectId: "pruebafirebase-92d46",
+    storageBucket: "pruebafirebase-92d46.appspot.com",
+    messagingSenderId: "1041826778083"
+  };
+  firebase.initializeApp(config);
