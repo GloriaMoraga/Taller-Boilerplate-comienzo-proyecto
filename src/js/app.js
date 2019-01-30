f33158c441aa0bfb59079e8037248e872be851b3
GloriaMoraga <silmeriel@gail.com>
2019-01-30 15:26:59 -0300

    integrando archivos

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+const miFuncionTesteable = () => {
+return true;
+
+}
---
+++ test/app.spec.js
@@ -0,0 +1,14 @@
+require('../src/js/app.js');
+const chai = require('chai');
+
+
+describe('Mostrar como iniciar un proyecto', () => {
+it('Deberia mostrar como hacer test', () => {
+    chai.assert.equal(miFuncionTesteable(),true)
+
+
+})
+
+
+
+})
